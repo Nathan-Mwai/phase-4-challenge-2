2df0a5f29aba53a5658619c9e9c0efa5d88f1198
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-11 12:52:04 +0300

    Adds serialization to RestaurantPizza Model and for ease in sight has space below

--- server/models.py
+++ server/models.py
@@ -23,6 +23,7 @@ class Restaurant(db.Model, SerializerMixin):
     pizzas = association_proxy('restaurant_pizzas', 'Pizza', creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
     # add serialization rules
     serialize_rules = ('-restaurant_pizzas.restaurant')
+    
     def __repr__(self):
         return f'<Restaurant {self.name}>'
 
@@ -40,6 +41,7 @@ class Pizza(db.Model, SerializerMixin):
 
     # add serialization rules
     serialize_rules = ('-restaurant_pizzas.pizza')
+    
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
 
@@ -56,7 +58,8 @@ class RestaurantPizza(db.Model, SerializerMixin):
     restaurant = db.relationship('Restaurant', back_populates="restaurant_pizzas")
     pizza = db.relationship('Pizza', back_populates="restaurant_pizzas")
     # add serialization rules
-
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
+    
     # add validation
 
     def __repr__(self):
