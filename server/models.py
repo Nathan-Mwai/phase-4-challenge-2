3b2a0b6a82fc38a13b18d7e452bb74b0479343c4
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-11 13:07:17 +0300

    finishes Models but updates will be done later.

--- server/models.py
+++ server/models.py
@@ -64,7 +64,7 @@ class RestaurantPizza(db.Model, SerializerMixin):
     @validates('price')
     def checking_price(self, key, price):
         if not 1 <= price <= 30:
-            raise ValueError('Price can only be between 1 and 30 not below 1 or above 30')
+            raise ValueError('Price can only be between 1 and 30')
         return price
             
     def __repr__(self):
